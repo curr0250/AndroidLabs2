89f7c2762838f78b6ca8bf9a8096051343f0134b
jojcurr <curr0250@algonquinlive.com>
2018-09-14 16:53:46 -0400

    Lab 2A with 3 activities image and manual activity change not working

---
+++ app/src/main/java/com/example/jodycurry/androidlabs/ListItemsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jodycurry.androidlabs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListItemsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_items)
+    }
+}
